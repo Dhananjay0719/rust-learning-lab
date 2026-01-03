7fb23641c982b5b52f0beff0d401f16200dc0931
Dhananjay Badoni <101263267+Dhananjay0719@users.noreply.github.com>
2026-01-03 16:29:56 +0530

    Create 01_even_odd.rs

---
+++ 01_even_odd.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let ans=is_even(11);
+    println!("{}",ans);
+}
+
+fn is_even(n:i32) -> bool {
+    if n%2==0{
+        return true;
+    }
+    else {
+        return false;
+    }
+}
