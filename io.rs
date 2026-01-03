be38e7339009978c11969797e4758f6771ef243c
Dhananjay Badoni <101263267+Dhananjay0719@users.noreply.github.com>
2026-01-03 18:38:39 +0530

    Create io.rs

---
+++ io.rs
@@ -0,0 +1,8 @@
+use std::io;
+
+fn main(){
+    let mut input=String::new();
+    println!("Enter Your Input:");
+    io::stdin().read_line(&mut input).expect("User Input Failed");
+    println!("User Input is {}",input);
+}
